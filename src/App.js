1d53097510f7b004efdaddf9718cea275df8f38c
Tobi Kudayisi <kudayisitobi@gmail.com>
2023-03-17 20:20:23 +0100

    added react router dom

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ import {
 } from "react-router-dom"
 
 import {BrowserRouter as Router} from 'react-router-dom'
-
-
-import logo from './logo.svg'
+import HomePage from "./pages/Home"
 import './App.css'
 
 function App() {
@@ -28,23 +26,5 @@ return (
 )
 }
 
-const HomePage = () => (
-   <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-)
 
 export default App;
