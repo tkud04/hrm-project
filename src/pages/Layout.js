2832d7989852ac1f7c49997ccd8797f1aaf33cd3
Tobi Kudayisi <kudayisitobi@gmail.com>
2023-03-19 18:01:11 +0100

    added sticky header

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -37,7 +37,7 @@ const Layout = () => {
         </button>
         {/* LOGO */}
         <a className="navbar-brand logo" href="landing.html">
-            <i className="fa fa-bitcoin"></i> TRADIFY
+            <i className="fa fa-bitcoin"></i> HRM DEMO
         </a>
     </div>
     {/* end navbar-header */}
@@ -46,39 +46,21 @@ const Layout = () => {
     <div className="navbar-collapse collapse" id="data-scroll">
         <ul className="nav navbar-nav navbar-right">
              <li className="active">
-                <a href="#home">Home</a>
+                <a href="/home">Home</a>
             </li>
-            <li className="dropdown">
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Landing <span className="caret"></span></a>
-            <ul className="dropdown-menu dropdown-menu-dark dropdown-left-0">
-              <li><a href="landing.html">Layout One</a></li>
-              <li><a href="landing-2.html">Layout Two</a></li>
-            </ul>
-          </li>
+           
             <li>
-                <a href="#About">About</a>
+                <a href="/about">About</a>
             </li>
 
             <li>
-                <a href="#Currencies">Currencies</a>
+                <a href="/contact">Contact</a>
             </li>
-            <li className="dropdown">
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Markets <span className="caret"></span></a>
-            <ul className="dropdown-menu dropdown-menu-dark dropdown-left-0">
-              <li><a href="index.html">Layout One</a></li>
-              <li><a href="index-2.html">Market Two</a></li>
-              <li><a href="index-3.html">Market Three</a></li>
-              <li><a href="index-4.html">Market Four</a></li>
-              <li><a href="index-5.html">Market Five</a></li>
-              <li><a href="index-6.html">Market Six</a></li>
-            </ul>
-          </li>
+           
             <li>
-                <a href="#blog">Blog</a>
+                <a href="/blog">Blog</a>
             </li>
-            <li>
-                <a href="datatable.html">Shortcodes</a>
-            </li>
+           
         </ul>
     </div>
     {/*/Menu */}
@@ -113,7 +95,7 @@ const Layout = () => {
                 <div className="row">
 
                     <div className="col-md-4 col-sm-12">
-                        <h4 className="logo"><i className="fa fa-bitcoin"></i> TRADIFY</h4>
+                        <h4 className="logo"><i className="fa fa-bitcoin"></i> HRM DEMO</h4>
                         <p>Architecto beatae vitae dicta sunt explicabo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel sapien et lacus tempus varius. In finibus lorem vel.</p>
 
                         <ul className="list-inline social">
