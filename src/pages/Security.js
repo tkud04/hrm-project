0d0b2d96248bb3ec22f7ce5f276cce5800a4280a
Tobi Kudayisi <32499735+tkud04@users.noreply.github.com>
2023-04-09 18:59:05 +0100

    Create Security.js

---
+++ src/pages/Security.js
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from "react"
+
+const Security = () => {
+  
+    return (
+      <div className="container-xxl py-5">
+        <div className="container">
+         <h4>Dashboard</h4>
+        </div>
+      </div>
+    )
+}
+
+export default Security
