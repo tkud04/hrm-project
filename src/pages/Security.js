bcac7c6d4acf68990cf1b0bbef3a5adec51e07c4
Tobi Kudayisi <32499735+tkud04@users.noreply.github.com>
2023-04-10 17:36:43 +0100

    Update Security.js

--- src/pages/Security.js
+++ src/pages/Security.js
@@ -3,6 +3,11 @@ import SideBar from "../components/SideBar"
 
 const Security = () => {
   
+   const ModalBody = () => (
+      <div>
+       <p>Testing modal body</p>
+      </div>
+   )
     return (
       <div className="row">
         <div className="col-md-4">
@@ -36,26 +41,11 @@ const Security = () => {
            </div>
           </div>
         </div>
-        <div className="col-md-12">
-      
-<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        ...
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-        </div>
+     <GenericModal
+      id='#exampleModal'
+      title='Change Password'
+      body={ModalBody}
+     />
       </div>
     )
 }
